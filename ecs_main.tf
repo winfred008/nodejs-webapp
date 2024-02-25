3890a92c958656daec469e3bda378d6834e89ab6
winfred008 <144569892+winfred008@users.noreply.github.com>
2024-02-25 11:14:03 -0500

    Create ecs_main.tf

---
+++ ecs_main.tf
@@ -0,0 +1,70 @@
+# Define the provider for AWS
+provider "aws" {
+  region = "us-east-2"  
+}
+
+# Create an ECS cluster
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "winfred008-ecs-cluster"  
+}
+
+# Create a task definition
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                = "my-task-family-test"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = <<EOF
+[
+  {
+    "name": "my-container",
+    "image": "ngnix:latest",  
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOF
+}
+
+# Create a service to run the task on the cluster
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = ["subnet-08840fce5e729bdce"]  
+    security_groups  = ["sg-09c8b66f444afe949"]      
+    assign_public_ip = true
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role-jan"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
